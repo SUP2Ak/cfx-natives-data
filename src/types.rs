483441a7d6818ec934ecfd60f78d78bfe810fb56
SUP2Ak <cormier.wesley@gmail.com>
2025-05-04 20:28:14 +0200

    ✨ feat(types): add PartialEq to GamesType enum
    - implement PartialEq for GamesType to allow comparison
    - enhance usability in game type comparisons

--- src/types.rs
+++ src/types.rs
@@ -1,7 +1,7 @@
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub enum GamesType {
     GTA5,
     RDR3,
