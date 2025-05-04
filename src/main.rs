7b3cf29ce2679a2c0bedf61a7481ab6fe363ed7a
SUP2Ak <cormier.wesley@gmail.com>
2025-05-04 20:28:07 +0200

    ✨ feat(main): enhance documentation fetching and processing
    - add fetching of markdown filenames for GTA5 and CFX games
    - log time taken to fetch documentation and total count
    - pass fetched documentation to save_natives for improved processing

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,36 @@ use std::time::Instant;
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     std::fs::create_dir_all("fetched")?;
-    let start_time = Instant::now();
     let parser = NativeParser::new();
     let natives_count = Arc::new(AtomicUsize::new(0));
-
+    let start_time_docs = Instant::now();
+    let gta5_docs = parser.fetch_markdown_filenames(GamesType::GTA5).await?;
+    let cfx_docs = parser.fetch_markdown_filenames(GamesType::CFX).await?;
+    let docs_join = [gta5_docs, cfx_docs].concat();
+    println!("Time taken to fetch docs: {:?} | {} Total docs fetched", start_time_docs.elapsed(), docs_join.len());
+    
+    let start_time = Instant::now();
     let handles = vec![
         {
             let parser = parser.clone();
             let count = natives_count.clone();
+            let docs_join = docs_join.clone();
             tokio::spawn(async move {
                 if let Ok(n) = parser
-                    .save_natives(GamesType::GTA5, "fetched/natives.gta5.json")
+                    .save_natives(GamesType::GTA5, "fetched/natives.gta5.json", docs_join)
+                    .await
+                {
+                    count.fetch_add(n, Ordering::SeqCst);
+                }
+            })
+        },
+        {
+            let parser = parser.clone();
+            let count = natives_count.clone();
+            let docs_join = docs_join.clone();
+            tokio::spawn(async move {
+                if let Ok(n) = parser
+                    .save_natives(GamesType::RDR3, "fetched/natives.rdr3.json", docs_join)
                     .await
                 {
                     count.fetch_add(n, Ordering::SeqCst);
@@ -28,19 +47,7 @@ async fn main() -> anyhow::Result<()> {
             let count = natives_count.clone();
             tokio::spawn(async move {
                 if let Ok(n) = parser
-                    .save_natives(GamesType::RDR3, "fetched/natives.rdr3.json")
-                    .await
-                {
-                    count.fetch_add(n, Ordering::SeqCst);
-                }
-            })
-        },
-        {
-            let parser = parser.clone();
-            let count = natives_count.clone();
-            tokio::spawn(async move {
-                if let Ok(n) = parser
-                    .save_natives(GamesType::CFX, "fetched/natives.cfx.json")
+                    .save_natives(GamesType::CFX, "fetched/natives.cfx.json", docs_join)
                     .await
                 {
                     count.fetch_add(n, Ordering::SeqCst);
@@ -48,7 +55,7 @@ async fn main() -> anyhow::Result<()> {
             })
         },
     ];
-
+    
     for handle in handles {
         handle.await?;
     }
@@ -69,5 +76,6 @@ async fn main() -> anyhow::Result<()> {
         gta5_count,
         rdr3_count
     );
+
     Ok(())
 }
